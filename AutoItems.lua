54b22f500c88265e99303a81dedc78e61c742b9a
Kornaukhov Server <kj2a@mail.ru>
2014-05-03 10:36:23 -0700

    add prm true/false

--- AutoItems.lua
+++ AutoItems.lua
@@ -1,4 +1,13 @@
 BsleepTick = nil
+
+-- Setting
+-- Enable Abuse Bottle?
+funcbottle = true
+-- Enable Auto Phase Boots?
+funcphaseboots = false
+-- Enable Auto Linken's Sphere?
+funclsphere = true
+
 function BTick( tick )
 	if not client.connected or client.loading or client.console then
 			return
@@ -21,15 +30,19 @@ function BTick( tick )
 		return
 	end
 	
-	if bottle and not me.invisible and not me:IsChanneling() and me:DoesHaveModifier("modifier_fountain_aura_buff") and not me:DoesHaveModifier("modifier_bottle_regeneration") then
+	if not funcbottle and not funcphaseboots and not funclsphere then
+		return
+	end
+	
+	if  funcbottle and bottle and not me.invisible and not me:IsChanneling() and me:DoesHaveModifier("modifier_fountain_aura_buff") and not me:DoesHaveModifier("modifier_bottle_regeneration") then
 		me:SafeCastItem("item_bottle")
 	end
 	
-	if phaseboots and me.alive == true and phaseboots.state == -1 and me.unitState ~= 33554432 and me.unitState ~= 256 and me.unitState ~= 33554688 then
+	if funcphaseboots and phaseboots and me.alive == true and phaseboots.state == -1 and me.unitState ~= 33554432 and me.unitState ~= 256 and me.unitState ~= 33554688 then
 		me:SafeCastItem("item_phase_boots")
 	end
 	
-	if lsphere and me.alive == true and lsphere.state == -1 and me.unitState ~= 33554432 and me.unitState ~= 256 and me.unitState ~= 33554688 then
+	if funclsphere and lsphere and me.alive == true and lsphere.state == -1 and me.unitState ~= 33554432 and me.unitState ~= 256 and me.unitState ~= 33554688 then
 		me:SafeCastItem("item_sphere",me)
 	end
 	
