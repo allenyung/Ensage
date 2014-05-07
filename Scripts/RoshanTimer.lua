7cb74d288752c9abd54b9aa3c7251e864de2694c
kj2a <kj2a@mail.ru>
2014-05-07 20:02:35 +0400

    Reload Fixed
    1. Bug: "Need Reload for work" Fixed
    2. Bug: "Roshan dead draw Alive" Fixed

--- Scripts/RoshanTimer.lua
+++ Scripts/RoshanTimer.lua
@@ -2,29 +2,34 @@ xPosition,yPosition = 220,5
 xPositionN,yPositionN = 220,20
 myFont = drawMgr:CreateFont("Roshancheg","Arial",13,400)
 StatusText = drawMgr:CreateText(xPosition,yPosition,0xFFFFFFFF,"",myFont);
-StatusText.visible = true
 InstructionTextToSpawn = drawMgr:CreateText(xPositionN,yPositionN,0xff0000ff,"Time, to respawn - To complete full respawn",myFont);
-InstructionTextToSpawn.visible = false
 InstructionTextSpawn = drawMgr:CreateText(xPositionN,yPositionN,0x00ff00ff,"Begun respawn time - To complete full respawn",myFont);
-InstructionTextSpawn.visible = false
+ReloadTextSpawn = drawMgr:CreateText(xPositionN,yPositionN,0xff0000ff,"The respawn time of unknown!",myFont);
 sleepTick = nil
 function Frame( tick )
 	if not client.connected or client.loading or client.console then
 		return
 	end
+	
+	local entities = entityList:FindEntities({classId=CDOTA_Unit_Roshan})
+	
 	if not deathTime then
-		StatusText.text = "Roshan: Alive"
-		InstructionTextToSpawn.visible = false
-		InstructionTextSpawn.visible = false
+		if #entities == 1 then
+			StatusText.text = "Roshan: Alive"
+			ReloadTextSpawn.visible=false
+		elseif #entities == 0 then
+			StatusText.text = "Roshan: Dead"
+			ReloadTextSpawn.visible = true
+			InstructionTextToSpawn.visible = false
+			InstructionTextSpawn.visible = false
+		end
 		return
 	end
 	
-	
-	local entities = entityList:FindEntities({classId=CDOTA_Unit_Roshan})
 	local tickDelta = client.gameTime-deathTime
 	local minutes = math.floor(tickDelta/60)
 	local seconds = tickDelta%60
-	local printMe 	
+	local printMe
 	
 	if client.gameTime > sleepTick and #entities > 0 then
 		local rosh = entities[1]
@@ -57,6 +62,7 @@ function ubiliroshana(tick)
 	deathTime = client.gameTime
 	sleepTick = client.gameTime+10
 	InstructionTextToSpawn.visible=true
+	ReloadTextSpawn.visible=false
 	if reakciyacheloveka == nil then
 		sleep = tick + 1000
 		reakciyacheloveka = 1
@@ -70,11 +76,30 @@ function ubiliroshana(tick)
 end
 
 function Close()
+	script:UnregisterEvent(Frame)
+	script:UnregisterEvent(Roshanchik)
 	deathTime = nil
 	InstructionTextToSpawn.visible = false
+	InstructionTextSpawn.visible = false
 	StatusText.visible = false
+	ReloadTextSpawn.visible = false
+	registered = false
 end
 
-script:RegisterEvent(EVENT_FRAME,Frame)
-script:RegisterEvent(EVENT_DOTA,Roshanchik)
+function Load()
+	if registered then return end
+	script:RegisterEvent(EVENT_FRAME,Frame)
+	script:RegisterEvent(EVENT_DOTA,Roshanchik)
+	InstructionTextToSpawn.visible = false
+	InstructionTextSpawn.visible = false
+	StatusText.visible = true
+	ReloadTextSpawn.visible = false
+	registered = true
+end
+
+if client.connected and not client.loading then
+	Load()
+end
+
 script:RegisterEvent(EVENT_CLOSE,Close)
+script:RegisterEvent(EVENT_LOAD,Load)
